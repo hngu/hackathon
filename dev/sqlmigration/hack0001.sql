00981a3fadc942e5aec86e94e656e77be76bca37
Feng Zheng <feng.zheng@freecause.com>
2011-12-30 14:26:14 -0500

    database table updated for wishlist

--- dev/sqlmigration/hack0001.sql
+++ dev/sqlmigration/hack0001.sql
@@ -2,10 +2,10 @@ USE hackathon;
 
 DROP TABLE IF EXISTS wishlist;
 CREATE TABLE wishlist (
-upc bigint(20) unsigned NOT NULL,
-price decimal(10,2) DEFAULT 0.00,
-product_name varchar(32) DEFAULT NULL,
-url varchar(512) DEFAULT NULL,
-date_time datetime DEFAULT NULL,
-PRIMARY KEY (upc)
+id int(32) UNSIGNED NOT NULL,
+product_name varchar(256) DEFAULT NULL,
+url varchar(2048) DEFAULT NULL,
+price decimal(6,2) DEFAULT 0.00,
+wish_date datetime DEFAULT NULL,
+PRIMARY KEY(id)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
